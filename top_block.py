00089f86ee97b44ff0e4cfc64c69dd3144358608
Ken King <kking935@vt.edu>
2019-07-24 12:27:39 -0400

    Updated transmit to use built-in gaussian noise generator

--- top_block.py
+++ top_block.py
@@ -3,8 +3,9 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Jul 11 12:40:30 2019
+# Generated: Wed Jul 24 12:20:56 2019
 ##################################################
+
 
 if __name__ == '__main__':
     import ctypes
@@ -16,17 +17,12 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from gnuradio import blocks
+from gnuradio import analog
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio import uhd
-from gnuradio import wxgui
-from gnuradio import zeromq
 from gnuradio.eng_option import eng_option
-from gnuradio.fft import window
 from gnuradio.filter import firdes
-from gnuradio.wxgui import fftsink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import time
@@ -48,80 +44,30 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Blocks
         ##################################################
-        self.zeromq_rep_sink_0 = zeromq.rep_sink(gr.sizeof_float, 1, 'tcp://*:9005', 100, False, -1)
-        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_c(
-        	self.GetWin(),
-        	baseband_freq=0,
-        	y_per_div=10,
-        	y_divs=10,
-        	ref_level=0,
-        	ref_scale=2.0,
-        	sample_rate=samp_rate,
-        	fft_size=1024,
-        	fft_rate=15,
-        	average=False,
-        	avg_alpha=None,
-        	title='FFT Plot',
-        	peak_hold=False,
-        )
-        self.Add(self.wxgui_fftsink2_0_0.win)
-        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
-        	self.GetWin(),
-        	baseband_freq=0,
-        	y_per_div=10,
-        	y_divs=10,
-        	ref_level=0,
-        	ref_scale=2.0,
-        	sample_rate=samp_rate,
-        	fft_size=1024,
-        	fft_rate=15,
-        	average=False,
-        	avg_alpha=None,
-        	title='FFT Plot',
-        	peak_hold=False,
-        )
-        self.Add(self.wxgui_fftsink2_0.win)
-        self.uhd_usrp_source_0 = uhd.usrp_source(
-        	",".join(("", "serial=313594E")),
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+        	",".join(("", "serial=313595A")),
         	uhd.stream_args(
         		cpu_format="fc32",
         		channels=range(1),
         	),
         )
-        self.uhd_usrp_source_0.set_clock_rate(30.72e6, uhd.ALL_MBOARDS)
-        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_source_0.set_center_freq(915e6, 0)
-        self.uhd_usrp_source_0.set_gain(100, 0)
-        self.fir_filter_xxx_0 = filter.fir_filter_ccc(1, (11, ))
-        self.fir_filter_xxx_0.declare_sample_delay(0)
-        self.blocks_nlog10_ff_0 = blocks.nlog10_ff(10, 1, 0)
-        self.blocks_moving_average_xx_0 = blocks.moving_average_ff(1000, 1, 4000, 1)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, 'power_raw', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
-        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
-
-
+        self.uhd_usrp_sink_0.set_clock_rate(30.72e6, uhd.ALL_MBOARDS)
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_center_freq(2450e6, 0)
+        self.uhd_usrp_sink_0.set_gain(50, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_moving_average_xx_0, 0))
-        self.connect((self.blocks_moving_average_xx_0, 0), (self.blocks_nlog10_ff_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.zeromq_rep_sink_0, 0))
-        self.connect((self.fir_filter_xxx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
-        self.connect((self.fir_filter_xxx_0, 0), (self.wxgui_fftsink2_0_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.fir_filter_xxx_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.uhd_usrp_sink_0, 0))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
-        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
-        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
 
 
 def main(top_block_cls=top_block, options=None):
