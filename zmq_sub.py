bb362e375578e262f3439889f073e376d46a855e
Matthew Foran <mjforan@vt.edu>
2019-08-05 14:38:03 -0400

    ros node updated

--- zmq_sub.py
+++ zmq_sub.py
@@ -1,42 +1,65 @@
 #!/usr/bin/env python
 
-import zmq
-import struct
-
 import rospy
 import durip.msg
 
 
-# Socket to talk to server
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
 
-socket.connect ("tcp://localhost:9005")
-rospy.loginfo("Connected")
+import zmq
+import struct
 
-rospy.init_node('uhdRX', disable_signals=True) #enable signals before integrating with main ROS code
+
+class ZMQfeed:
+        def __init__(self):
+                # Socket to talk to server
+                self.context = zmq.Context()
+                self.socket = self.context.socket(zmq.REQ)
+
+                self.socket.connect ("tcp://localhost:9005")
+
+                self.rssiVal = 0
+
+
+        def run(self):
+		try:
+                        self.socket.send("request")
+                        tcp_data=self.socket.recv()
+                        self.rssiVal=struct.unpack('f', tcp_data[0:4])[0]
+		except struct.error:
+			pass
+
+        def get_data(self):
+                return self.rssiVal
+
+        def __del__(self):
+                self.socket.close()
+                self.context.term()
+
+
+
+
+
+
+
+
+rospy.init_node('uhdRX')
 
 rssi_pub = rospy.Publisher("rssi", durip.msg.rssiMsg, queue_size = 1)
 
-rssiVal = 0
+zmq = ZMQfeed()
+
 
 
 def rosPub(event):
+	rssiVal = zmq.get_data() 
 	pubMsg = durip.msg.rssiMsg(timestamp=rospy.Time.now().secs, rssi=rssiVal)
         rssi_pub.publish(pubMsg)
 
 
+
+
 rospy.Timer(rospy.Duration(0.01), rosPub)
 
-while True:
-	try:
-       		socket.send("request")
-		tcp_data=socket.recv()
-		rssiVal=struct.unpack('f', tcp_data[0:4])[0]
-	except KeyboardInterrupt:
-		break
-	except:
-		pass
-socket.close()
-context.term()
 
+while True:
+	zmq.run()
